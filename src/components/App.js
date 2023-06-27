9a3ef7aaf8e7929390c57d0f8dd1b39bf23a2eee
Kyle Crawford <kcraw217@gmail.com>
2023-06-27 11:10:37 -0700

    add header

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import MarketControl from "./MarketControl";
+
+function App(){
+  return(
+    <>
+    <Header />
+    <MarketControl />
+    </>
+  );
+}
+
+export default App;
