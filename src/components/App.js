766dc0fde55c5758af2114612c13f235453a7a69
Kyle Crawford <kcraw217@gmail.com>
2023-06-27 15:27:52 -0700

    add bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.css'
 import Header from "./Header";
 import MarketControl from "./MarketControl";
 import ProduceControl from "./ProduceControl";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-function App(){
-  return(
+function App() {
+  return (
     <>
-    <Header />
-    <MarketControl />
-    <ProduceControl />
+      <Container>
+        <Header />
+        <hr></hr>
+        <Row>
+          <Col><MarketControl /></Col>
+          <Col><ProduceControl /></Col>
+        </Row>
+      </Container>
     </>
   );
 }
